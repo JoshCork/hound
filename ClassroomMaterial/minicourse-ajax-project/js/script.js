3ec4666a97723dd0b896e8ef068bb7ba876430d7
Joshua Cork <joshua.cork@gmail.com>
2015-03-19 22:37:19 -0700

    added new folder with the new challenge from the course.  Update the javascript to pull from the NYT API and sort through the JSON to add list items to an unordered list on the html page.

---
+++ ClassroomMaterial/minicourse-ajax-project/js/script.js
@@ -0,0 +1,60 @@
+
+// myNYT key: 3cc303338d57062cab3b82000286ca48:2:71622028 - article search
+// myNYT geo Key: d9d00c130383e3546b7c1df6f3b715c1:11:71622028
+ 
+
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+
+
+    // load streetview
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+
+    // load nytimes
+    
+    //"http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=3cc303338d57062cab3b82000286ca48:2:71622028&fq=glocation:('Arizona')
+    ////http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=glocation%3A%28%27Arizona%27%29&api-key=3cc303338d57062cab3b82000286ca48%253A2%253A71622028
+    
+    $.getJSON("http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=glocations:(" + cityStr + ")&api-key=3cc303338d57062cab3b82000286ca48%3A2%3A71622028",function ( data ) {
+        var items = [];
+        //$.each( data.response.docs,function(key,val) { items.push("li id='" + key + "'>" + val + "</li>" );});
+        // $.each(data.response.docs, function(key,val) { 
+        //     items.push(key + val); 
+        // });
+
+        $.each(data.response.docs,function(key,val) {   
+                items.push("<li id='" + key + "'>" + this.headline.main + "</li>");                
+         });
+
+        console.log(items);       
+
+        $("<ul/>", {
+            "class" : "my-new-list",
+            html: items.join( "" )
+        }).appendTo( "body" );
+
+    });
+
+
+
+    return false;
+};
+
+$('#form-container').submit(loadData);
--- ClassroomMaterial/minicourseAjaxProject/js/script.js
+++ ClassroomMaterial/minicourseAjaxProject/js/script.js
@@ -1,4 +1,7 @@
 
+// myNYT key: 3cc303338d57062cab3b82000286ca48:2:71622028 - article search
+// myNYT geo Key: d9d00c130383e3546b7c1df6f3b715c1:11:71622028
+
 function loadData() {
 
     var $body = $('body');
@@ -17,20 +20,28 @@ function loadData() {
     // The code commented out below will pull out the city and street 
     // text and create a url using my key.
 
-    var myCityText = NULL
-    var myStreetText = NULL
-    var myUrl = "https://maps.googleapis.com/maps/api/streetview?size=400x400&location="
-    var myKey = "AIzaSyCo7SZK2iqRwqZACNN9kP8ssQbpUhKQvXM"
+    var myCityText;
+    var myStreetText;
+    var myUrl = "https://maps.googleapis.com/maps/api/streetview?size=1200x800&location=";
+    var myKey = "AIzaSyCo7SZK2iqRwqZACNN9kP8ssQbpUhKQvXM";
 
-    myStreetText = $("#street").val()
-    myCityText = $("#city").val()
+    myStreetText = $("#street").val();
+    myCityText = $("#city").val();
+    var address = myStreetText + ', ' + myCityText;
 
-    myUrl = myUrl + myStreetText + myCityText + "&key=" + myKey
 
-    $body.append('<img class="bgimg" src="'+ myUrl + '">');
+    myUrl = myUrl + address + "&key=" + myKey;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+    $body.append('<img class="bgimg" src="' + myUrl + '">');
+    
+    // NYTimes AJAX Stuff
+    $.getJSON("http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=3cc303338d57062cab3b82000286ca48:2:71622028",function ( data ) {
+        console.log(data);
+    });
 
     return false;
-};
+}
 
 $('#form-container').submit(loadData);
 
