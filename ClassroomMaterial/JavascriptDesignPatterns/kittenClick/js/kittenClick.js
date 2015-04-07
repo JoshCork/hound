539d42f7a81b198642600d267b0e845fd5ded423
JoshCork <joshua.cork@gmail.com>
2015-04-06 20:47:11 -0700

    added some new files

---
+++ ClassroomMaterial/JavascriptDesignPatterns/kittenClick/js/kittenClick.js
@@ -0,0 +1,7 @@
+var clicks
+
+$('#images').click(function(e) {
+	clicks++;
+	console.log("I have been clicked " + clicks + " times!");
+  //the element has been clicked... do stuff here
+});
---
+++ ClassroomMaterial/JavascriptDesignPatterns/kittenClick/js/redditImages.js
@@ -0,0 +1,23 @@
+$.getJSON('http://www.reddit.com/r/kittens/.json?jsonp=?&show=all&limit=300', function (data) {
+    $.each(data.data.children, function (i, item) {
+        IsValidImageUrl(item.data.url, function (url, isvalid) {
+            if (isvalid) {
+                $('<img/>').attr('src', item.data.url)
+                    .width(500)
+                    .appendTo('#images');
+            }
+        });
+    });
+});
+
+
+function IsValidImageUrl(url, callback) {
+    var img = new Image();
+    img.onerror = function () {
+        callback(url, false);
+    };
+    img.onload = function () {
+        callback(url, true);
+    };
+    img.src = url;
+}
