b1c0bc25cec67100038baa9360198d80abd57eca
JoshCork <joshua.cork@gmail.com>
2015-04-08 16:08:41 -0700

    stopped putting reddit images on the page and isntead put them in a variable

--- ClassroomMaterial/JavascriptDesignPatterns/kittenClick/js/redditImages.js
+++ ClassroomMaterial/JavascriptDesignPatterns/kittenClick/js/redditImages.js
@@ -1,23 +1,30 @@
-$.getJSON('http://www.reddit.com/r/catpictures/.json?jsonp=?&show=all&limit=300', function (data) {
-    $.each(data.data.children, function (i, item) {
-        IsValidImageUrl(item.data.url, function (url, isvalid) {
+var imgArray = []
+
+$.getJSON('http://www.reddit.com/r/catpictures/.json?jsonp=?&show=all&limit=300', function(data) {
+    $.each(data.data.children, function(i, item) {
+        IsValidImageUrl(item.data.url, function(url, isvalid) {
             if (isvalid) {
-                $('<img/>').attr('src', item.data.url)
-                    .width(500)
-                    .appendTo('#images');
-            }
+                imgArray.push(item.data.url);
+                // $('<img/>').attr('src', item.data.url)
+                //     .width(500)
+                //     .appendTo('#images');
+            }            
         });
     });
+console.log("test");
 });
+
+//console.log(imgArray);
 
 
 function IsValidImageUrl(url, callback) {
     var img = new Image();
-    img.onerror = function () {
+    img.onerror = function() {
         callback(url, false);
     };
-    img.onload = function () {
+    img.onload = function() {
         callback(url, true);
     };
     img.src = url;
+    console.log("I am img.src: " + img.src);
 }
