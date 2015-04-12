5961d2672052ea37f7eb3625c213a718c6ddb884
Joshua Cork <joshua.cork@gmail.com>
2015-04-11 22:30:31 -0700

    Moved ajax call to reddit back to being async. created a way to count the iterations through the list of urls and to have them do something once they were all successfully iterated through.

--- ClassroomMaterial/JavascriptDesignPatterns/kittenClick/js/redditImages.js
+++ ClassroomMaterial/JavascriptDesignPatterns/kittenClick/js/redditImages.js
@@ -1,17 +1,23 @@
 var imgArray = [];
 var urlArray = [];
-var redditData;
+var callbackCounter = 0;
 
 
 
 function parseUrls() {
     $.each(urlArray, function(i, item) {
         IsValidImageUrl(item, function(url, isvalid) {
+            callbackCounter++;
+            // console.log(callbackCounter);
+            if (callbackCounter >= urlArray.length) {
+                console.log("imgArray has completed. Length:" + imgArray.length);
+                console.log(imgArray);
+            }
             if (isvalid) {
                 imgArray.push(item);
-                // $('<img/>').attr('src', item[i])
-                //     // .width(500)
-                //     .appendTo('#images');
+                $('<img/>').attr('src', item)
+                    .width(500)
+                    .appendTo('#images');
             }
         });
     });
@@ -19,86 +25,66 @@ function parseUrls() {
 
 
 function IsValidImageUrl(url, callback) {
-    var img = new Image();
-    img.onerror = function() {
-        callback(url, false);
-    };
-    img.onload = function() {
-        callback(url, true);
-    };
-    img.src = url;
-}
-
-console.log("urlArray: " + urlArray);
-
-// $.ajax({
-//     async: false,
-//     url: "http://www.reddit.com/r/catpictures/.json?jsonp=?&show=all&limit=50",
-//     dataType: "json",
-//     success: function(jsonData) {
-//         redditData = jsonData;
-
-//         $.each(jsonData.data.children, function(i, item) {
-//             urlArray.push(item.data.url);
-//         });
-
-//         //     $.each(urlArray, function(i, item) {
-//         //     IsValidImageUrl(item, function(url, isvalid) {
-//         //         if (isvalid) {
-//         //             imgArray.push(item);
-//         //             // $('<img/>').attr('src', item[i])
-//         //             //     // .width(500)
-//         //             //     .appendTo('#images');
-//         //         }
-//         //     });
-//         // });
 
 
-//     },
-//     error: function() {
-//         console.log("oh snap!");
-//     },
-//     complete: function() {
-//         console.log("urlArray Length: " + urlArray.length);
-//         console.log("urlData is: " + urlArray);
-//         console.log("1st redditData is: " + redditData);
-//         console.log("imgArray: " + imgArray);
+            var arr = ["jpeg", "jpg", "gif", "png"];
+            var ext = url.substring(url.lastIndexOf(".") + 1);
 
-//     },
-
-// });
-
-function loadData() {
-    $.getJSON('http://www.reddit.com/r/catpictures/.json?jsonp=?&show=all&limit=50', function(data) {
-        $.each(data.data.children, function(i, item) {
-            urlArray.push(item.data.url);
-            console.log("I have pushed a URL");
-        });
-        console.log("i have updated the Array");
-    });
-
-
-    console.log("i'm after the $each and urlArray should be populated now");
-    console.log("urlArray: " + urlArray);
-}
-
-
-loadData();
+            if ($.inArray(ext, arr) > 0) {
+                    callback(url, true);
+                } else {
+                    callback(url, false);
+                }
 
 
 
+            // var img = new Image();
+            // img.onerror = function() {
+            //     callback(url, false);
+            // };
+            // img.onload = function() {
+            //     callback(url, true);
+            // };
+            // img.src = url;
+
+        }
+
+        console.log("urlArray: " + urlArray);
 
 
 
-// ------------------------------------
+        function loadData() {
 
-// function(url, isvalid) {
-//             if (isvalid) {
-//                 imgArray.push(item.data.url);
-//                 $('<img/>').attr('src', item.data.url)
-//                     .width(500)
-//                     .appendTo('#images');
-//             }
+            $.ajax({
+                // async: false,
+                url: "http://www.reddit.com/r/catpictures/.json?jsonp=?&show=all&limit=50",
+                dataType: "json",
+                success: function(jsonData) {
+                    redditData = jsonData;
+
+                    $.each(jsonData.data.children, function(i, item) {
+                        urlArray.push(item.data.url);
+                    });
+
+                    // console.log("the each loop has completed");
+                    parseUrls();
+                    // console.log("parse URL's has been called");
 
 
-// .done(function(){console.log("done done done");})
+                },
+                error: function() {
+                    console.log("oh snap!");
+                },
+                // complete: function() {
+                //     console.log("urlArray Length: " + urlArray.length);
+                //     console.log("urlData is: " + urlArray);
+                //     console.log("imgArray: " + imgArray);
+
+                // },
+
+            });
+        }
+
+
+        loadData();
+
