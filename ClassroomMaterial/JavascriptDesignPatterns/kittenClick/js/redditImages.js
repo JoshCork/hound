95c396f1d6f8de85aed6bcad60ef8af80e51a7ef
Joshua Cork <joshua.cork@gmail.com>
2015-04-11 23:08:15 -0700

    Seeet!  It's working now!   I can pull images from Reddit, i randomly shuffle them, and display the first two from the shuffle on the page. Super cool!

--- ClassroomMaterial/JavascriptDesignPatterns/kittenClick/js/redditImages.js
+++ ClassroomMaterial/JavascriptDesignPatterns/kittenClick/js/redditImages.js
@@ -1,90 +1,90 @@
 var imgArray = [];
 var urlArray = [];
-var callbackCounter = 0;
+
+
+function IsValidImageUrl(url) {
+
+    // modified based on reading through this stackoverflow article: http://bit.ly/1PzGc0b
+    var arr = ["jpeg", "jpg", "gif", "png"];
+    var ext = url.substring(url.lastIndexOf(".") + 1);
+    var isValid;
+
+    if ($.inArray(ext, arr) > 0) {
+        isValid = true;
+    } else {
+        isValid = false;
+    }
+
+    return isValid;
+
+}
+
+// pulled from: http://bit.ly/1CFUQZF
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 
 
-function parseUrls() {
-    $.each(urlArray, function(i, item) {
-        IsValidImageUrl(item, function(url, isvalid) {
-            callbackCounter++;
-            // console.log(callbackCounter);
-            if (callbackCounter >= urlArray.length) {
-                console.log("imgArray has completed. Length:" + imgArray.length);
-                console.log(imgArray);
-            }
-            if (isvalid) {
-                imgArray.push(item);
-                $('<img/>').attr('src', item)
-                    .width(500)
-                    .appendTo('#images');
-            }
-        });
+
+
+function loadData() {
+
+    $.ajax({
+        // async: false,
+        url: "http://www.reddit.com/r/catpictures/.json?jsonp=?&show=all&limit=50",
+        dataType: "json",
+        success: function(jsonData) {
+            redditData = jsonData;
+            var arr = ["jpeg", "jpg", "gif", "png"];
+
+            $.each(jsonData.data.children, function(i, item) {
+                var url = item.data.url;
+
+                if (IsValidImageUrl(url)) {
+                    imgArray.push(url);
+                    // only if you want to put all the images on the screen.
+                    // $('<img/>').attr('src', url).width(500).appendTo('#images');
+                } else {
+                    // do nothing.
+                }
+
+            });
+
+            imgArray = shuffle(imgArray);
+            $('<img/>').attr('src', imgArray[0]).width(500).appendTo('#images');
+            $('<img/>').attr('src', imgArray[1]).width(500).appendTo('#images');
+
+
+
+        },
+        error: function() {
+            console.log("oh snap!");
+        },
+        // complete: function() {
+        //     console.log("urlArray Length: " + urlArray.length);
+        //     console.log("urlData is: " + urlArray);
+        //     console.log("imgArray: " + imgArray);
+
+        // },
+
     });
 }
 
 
-function IsValidImageUrl(url, callback) {
-
-
-            var arr = ["jpeg", "jpg", "gif", "png"];
-            var ext = url.substring(url.lastIndexOf(".") + 1);
-
-            if ($.inArray(ext, arr) > 0) {
-                    callback(url, true);
-                } else {
-                    callback(url, false);
-                }
-
-
-
-            // var img = new Image();
-            // img.onerror = function() {
-            //     callback(url, false);
-            // };
-            // img.onload = function() {
-            //     callback(url, true);
-            // };
-            // img.src = url;
-
-        }
-
-        console.log("urlArray: " + urlArray);
-
-
-
-        function loadData() {
-
-            $.ajax({
-                // async: false,
-                url: "http://www.reddit.com/r/catpictures/.json?jsonp=?&show=all&limit=50",
-                dataType: "json",
-                success: function(jsonData) {
-                    redditData = jsonData;
-
-                    $.each(jsonData.data.children, function(i, item) {
-                        urlArray.push(item.data.url);
-                    });
-
-                    // console.log("the each loop has completed");
-                    parseUrls();
-                    // console.log("parse URL's has been called");
-
-
-                },
-                error: function() {
-                    console.log("oh snap!");
-                },
-                // complete: function() {
-                //     console.log("urlArray Length: " + urlArray.length);
-                //     console.log("urlData is: " + urlArray);
-                //     console.log("imgArray: " + imgArray);
-
-                // },
-
-            });
-        }
-
-
-        loadData();
-
+loadData();
